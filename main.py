c3810372204ea800cbfa39f8b70093d541a47fee
Vasudev V <vasudev20057d@gmail.com>
2026-03-29 16:01:51 +0530

    Remove hardcoded database password
    Removed hardcoded database password for security.

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Vasu2005",
+        password="",    #mysqlpasswordhere
         database="Attandance"
     )
 
